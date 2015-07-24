94428ddd94df87a93e1e4d508837b14f8984a5b5
Lum <ibmt20@hotmail.com>
2015-07-24 15:05:24 +0800

    Update

--- app/models/tqrdc/order.rb
+++ app/models/tqrdc/order.rb
@@ -1,4 +1,46 @@
 class Tqrdc::Order < ActiveRecord::Base
   has_many :ordergroups, :class_name => 'Tqrdc::OrderGroup', :dependent => :destroy
-  has_many :orderlines, :class_name => 'Tqrdc::OrderLine' , :dependent => :destroy
+  has_many :orderlines, :class_name => 'Tqrdc::OrderLine', :dependent => :destroy
+
+  validates_uniqueness_of :supplier_id, :scope => [:period]
+
+  def self.monthly_create_order
+    period = Time.now.strftime('%Y%m')
+    suppliers = Tqrdc::Supplier
+                    .where("'#{period}' between start_period and end_period")
+                    .order(:territory => :asc, :supplier => :asc)
+    suppliers.each do |supplier|
+      order = Tqrdc::Order.create(
+          :supplier_id => supplier.id,
+          :period => period,
+          :status => 'CREATE',
+          :territory => supplier.territory,
+          :total => 0
+      )
+
+      Tqrdc::Group.all.order(:id => :asc).each do |group|
+        order_group = Tqrdc::OrderGroup.create(
+            :order_id => order.id,
+            :group_id => group.id,
+            :status => 'CREATE',
+            :total => 0
+
+        )
+        qheads = Tqrdc::Qhead.where(:group_id => group.id).order(:name => :asc)
+        qheads.each do |qhead|
+          Tqrdc::OrderLine.create(
+              :order_id => order.id,
+              :order_group_id => order_group.id,
+              :status => 'CREATE',
+              :qhead_id => qhead.id,
+              :final_score => 0
+          )
+        end #qheads.each do |qhead|
+
+      end #Tqrdc::Group.all.order(:id => :asc).each do |group|
+
+    end #suppliers.each do |supplier|
+
+  end
+
 end
