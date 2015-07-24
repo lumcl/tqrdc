5692d410c25a0c6afcec23d4229eecad6aac6531
Lum <ibmt20@hotmail.com>
2015-07-24 18:22:04 +0800

    Update

--- app/models/tqrdc/qline.rb
+++ app/models/tqrdc/qline.rb
@@ -2,7 +2,7 @@ class Tqrdc::Qline < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   belongs_to :creator, :class_name => 'User'
   belongs_to :updater, :class_name => 'User'
-  belongs_to :qheads ,:class_name => 'Tqrdc::Qhead'
+  belongs_to :qhead ,:class_name => 'Tqrdc::Qhead'
 
   def score_with_name
     "[ #{score.to_i} ] - #{name}"
