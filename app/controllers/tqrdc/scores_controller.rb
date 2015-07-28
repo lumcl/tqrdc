3102f2ab76e58a1f1b0820a871ef075682b50091
Lum <ibmt20@hotmail.com>
2015-07-28 12:03:48 +0800

    Update

--- app/controllers/tqrdc/scores_controller.rb
+++ app/controllers/tqrdc/scores_controller.rb
@@ -1,10 +1,24 @@
 class Tqrdc::ScoresController < ApplicationController
 
   def supplier_entry
-    @order = Tqrdc::Order
-      .where(:supplier_id => current_user.supplier_id)
-      .where(:status => 'CREATE')
-      .last
+    @order = Tqrdc::Order.includes(:order_groups, :order_lines)
+                 .where(:supplier_id => current_user.supplier_id)
+                 .where(:status => 'CREATE')
+                 .last
+
+    @order_groups = @order.order_groups
+    @order_lines = @order.order_lines
+
+    @qheads = Hash.new
+    Tqrdc::Qhead.all.each do |qhead|
+      @qheads[qhead.id] = qhead
+    end
+
+    @groups = Hash.new
+    Tqrdc::Group.all.each do |group|
+      @groups[group.id] = group
+    end
+
   end
 
   def supplier_enquiry
