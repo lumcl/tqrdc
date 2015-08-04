1f6c1ce87b0319863d87d2c568001b0c5b71e299
minheeting <masusd@gmail.com>
2015-08-04 12:20:52 +0800

    Update

--- app/models/tqrdc/flow.rb
+++ app/models/tqrdc/flow.rb
@@ -2,4 +2,6 @@ class Tqrdc::Flow < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User' #, :foreign_key => 'owener_id', :primary_key => 'rid'
   belongs_to :creator, :class_name => 'User'
   belongs_to :updater, :class_name => 'User'
+
+  validates_presence_of :name , :qhead_id , :role , :territory , :seq , :next_seq
 end
