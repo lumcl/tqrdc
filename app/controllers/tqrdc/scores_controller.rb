dcb58bd03cad9d90d420596830f59a546bdf1044
Lum <ibmt20@hotmail.com>
2015-08-13 14:14:59 +0800

    Update

--- app/controllers/tqrdc/scores_controller.rb
+++ app/controllers/tqrdc/scores_controller.rb
@@ -16,7 +16,7 @@ class Tqrdc::ScoresController < ApplicationController
         join tqrdc_supplier b on b.id = a.supplier_id and b.supplier like '%#{supplier}%'
         join users u on u.id = b.commodity_mgr_id
         join tqrdc_order_line c on c.order_id = a.id and c.status <> 'CLOSE' and
-             (c.u#{current_user.id}_user_id = #{current_user.id} or c.u2_user_id = #{current_user.id} or c.u3_user_id = #{current_user.id} or c.u4_user_id = #{current_user.id})
+             (c.u1_user_id = #{current_user.id} or c.u2_user_id = #{current_user.id} or c.u3_user_id = #{current_user.id} or c.u4_user_id = #{current_user.id})
       where a.status <> 'CLOSE' and a.period like '%#{period}%' and a.territory like '%#{territory}%'
       order by a.period,a.territory,b.mtype,b.supplier
     "
@@ -30,17 +30,50 @@ class Tqrdc::ScoresController < ApplicationController
 
 
   def edit
-    @order = Tqrdc::Order.includes(:order_groups, :order_lines).find params[:id]
+    #@order = Tqrdc::Order.includes(:order_groups, :order_lines).find params[:id]
+    user_selections = "
+      ((tqrdc_order_line.u1_user_id = #{current_user.id}) or
+       (tqrdc_order_line.u2_user_id = #{current_user.id}) or
+       (tqrdc_order_line.u3_user_id = #{current_user.id}) or
+       (tqrdc_order_line.u4_user_id = #{current_user.id}))
+    "
+    @order = Tqrdc::Order
+                 .eager_load(:order_groups, :order_lines)
+                 .where('tqrdc_order_group.id = tqrdc_order_line.order_group_id')
+                 .where(user_selections)
+                 .find params[:id]
 
     @qheads = {}
     Tqrdc::Qhead.all.each do |qhead|
       @qheads[qhead.id] = qhead
     end
 
-    @groups = Hash.new
+    @groups = {}
     Tqrdc::Group.all.each do |group|
       @groups[group.id] = group
     end
+
+    sql = "
+      select distinct '1' utype,username from tqrdc_order_line a join users u on u.id = a.u1_user_id where a.order_id = #{params[:id]}
+      union
+      select distinct '2',username from tqrdc_order_line a join users u on u.id = a.u2_user_id where a.order_id = #{params[:id]}
+      union
+      select distinct '3',username from tqrdc_order_line a join users u on u.id = a.u3_user_id where a.order_id = #{params[:id]}
+      union
+      select distinct '4',username from tqrdc_order_line a join users u on u.id = a.u4_user_id where a.order_id = #{params[:id]}
+    "
+    @u1_users = []
+    @u2_users = []
+    @u3_users = []
+    @u4_users = []
+
+    Tqrdc::Order.find_by_sql(sql).each do |row|
+      @u1_users.append row.username if row.utype == '1'
+      @u2_users.append row.username if row.utype == '2'
+      @u3_users.append row.username if row.utype == '3'
+      @u4_users.append row.username if row.utype == '4'
+    end
+
   end
 
   def supplier_entry
--- app/models/tqrdc/order_line.rb
+++ app/models/tqrdc/order_line.rb
@@ -2,5 +2,8 @@ class Tqrdc::OrderLine < ActiveRecord::Base
   belongs_to :order_group, :class_name => 'Tqrdc::OrderGroup'
   belongs_to :qhead, :class_name => 'Tqrdc::Qhead'
   belongs_to :order, :class_name => 'Tqrdc::Order'
-
+  belongs_to :u1_user, :class_name => 'User'
+  belongs_to :u2_user, :class_name => 'User'
+  belongs_to :u3_user, :class_name => 'User'
+  belongs_to :u4_user, :class_name => 'User'
 end
