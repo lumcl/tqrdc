9ffdf8f2c0910a4af33ca6d07618ce729c54f138
Lum <ibmt20@hotmail.com>
2015-08-21 14:51:51 +0800

    Update

--- app/controllers/tqrdc/reports_controller.rb
+++ app/controllers/tqrdc/reports_controller.rb
@@ -1,5 +1,6 @@
 class Tqrdc::ReportsController < ApplicationController
   before_action :order_detail, only: [:show]
+
   def order_summary
     @orders, @groups = Tqrdc::Report.order_summary(params)
   end
@@ -8,7 +9,27 @@ class Tqrdc::ReportsController < ApplicationController
     @order = Tqrdc::Order.where(:seq => '5')
                  .eager_load(:order_groups, :order_lines)
                  .find params[:id]
+
+    sql =
+    "
+      select a.id,a.total,b.name,to_char(description) description,b.score
+      from tqrdc_order_group a
+        join tqrdc_group b on b.id = a.group_id
+      where a.order_id = #{params[:id]}
+      order by name desc
+    "
+    @labels = []
+    @values = []
+    order_groups = Tqrdc::Order.find_by_sql sql
+    order_groups.each do |row|
+      text = "#{row.name}"
+      @labels.append "'#{text}'"
+      @values.append row.total
+    end
+
   end
+
+  private
 
   def order_detail
     @qheads = {}
